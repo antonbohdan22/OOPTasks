adbffd5e425f236029f1749981c4b87ce14b7fc0
antonbohdan22 <antonarteezy22@gmail.com>
2020-12-07 05:05:36 +0200

    add shop

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,24 +6,35 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Operation
+    class Calculator
     {
-        public static double Add(double addend1, double addend2)
+
+        private Calculator() { }
+
+        private static Calculator calculator;
+
+        public static Calculator TurnOn()
+        {
+            if (calculator == null) return new Calculator();
+            return calculator;
+        }
+
+        public double Add(double addend1, double addend2)
         {
             return addend1 + addend2;
         }
 
-        public static double Subtract(double minuend, double subtrahend)
+        public double Subtract(double minuend, double subtrahend)
         {
             return minuend - subtrahend;
         }
 
-        public static double Multiply(double multiplier, double mulitplicanda)
+        public double Multiply(double multiplier, double mulitplicanda)
         {
             return multiplier * mulitplicanda;
         }
 
-        public static double Divide(double divident, double divisor)
+        public double Divide(double divident, double divisor)
         {
             return divident / divisor;
         }
--- Calculator/Runner.cs
+++ Calculator/Runner.cs
@@ -10,12 +10,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Calculator calculator = Calculator.TurnOn();
+
             int a = 30;
             int b = 8;
-            Console.WriteLine("Результат сложения {0} и {1}: {2}", a, b, Calculator.Add(a, b));
-            Console.WriteLine("Результат вычитания {0} и {1}: {2}", a, b, Calculator.Subtract(a, b));
-            Console.WriteLine("Результат умножения {0} и {1}: {2}", a, b, Calculator.Multiply(a, b));
-            Console.WriteLine("Результат деления {0} и {1}: {2}", a, b, Calculator.Divide(a, b));
+            Console.WriteLine("Результат сложения {0} и {1}: {2}", a, b, calculator.Add(a, b));
+            Console.WriteLine("Результат вычитания {0} и {1}: {2}", a, b, calculator.Subtract(a, b));
+            Console.WriteLine("Результат умножения {0} и {1}: {2}", a, b, calculator.Multiply(a, b));
+            Console.WriteLine("Результат деления {0} и {1}: {2}", a, b, calculator.Divide(a, b));
 
             Console.ReadKey();
         }
